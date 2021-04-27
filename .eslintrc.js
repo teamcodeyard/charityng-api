495136d8191cb077be6dacea1576d94b7ce2f680
Imre Ujlaki <iujlaki@codeyard.eu>
2021-04-27 09:24:03 +0200

    :wrench: Modified ESLint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,30 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es6": true,
-        "jquery": false,
-        "jest": true,
-        "jasmine": true
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'template-curly-spacing': 'off',
+    indent: 'off',
+  },
+  plugins: ['vue'],
+  overrides: [
+    {
+      files: 'src/**',
+      excludedFiles: '',
+      rules: {
+        'max-len': [2, 120, 4, { ignoreUrls: true }],
+      },
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module",
-        "ecmaVersion": "2018"
-    },
-    "rules": {
-        "indent": [
-            "warn",
-            "tab",
-            { "SwitchCase": 1 }
-        ],
-        "quotes": [
-            "warn",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-var": [
-            "error"
-        ],
-        "no-console": [
-            "off"
-        ],
-        "no-unused-vars": [
-            "warn"
-        ],
-        "no-mixed-spaces-and-tabs": [
-            "warn"
-        ]
-    }
+  ],
 };
